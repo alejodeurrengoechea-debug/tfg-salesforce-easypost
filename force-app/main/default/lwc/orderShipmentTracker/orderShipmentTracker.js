04892cf14288197b7cef17c9b8e7a663ba31ccba
alejodeurrengoechea-debug <alejodeurrengoechea@gmail.com>
2026-06-09 15:22:18 +0200

    feat: Sprint 4 - Mejoras UX componente orderShipmentTracker
    - Icono dinamico por estado (DELIVERED, IN_TRANSIT, etc.)
    - Badge de estado con color segun el ultimo evento
    - Tracking Number clicable con URL del carrier correspondiente
    - CSS personalizado para badges con colores SLDS

--- force-app/main/default/lwc/orderShipmentTracker/orderShipmentTracker.js
+++ force-app/main/default/lwc/orderShipmentTracker/orderShipmentTracker.js
@@ -1,6 +1,32 @@
 import { LightningElement, api, wire } from "lwc";
 import getShipmentData from "@salesforce/apex/OrderShipmentTrackerController.getShipmentData";
 
+const STATUS_ICON_MAP = {
+    DELIVERED:        "utility:success",
+    IN_TRANSIT:       "utility:truck",
+    OUT_FOR_DELIVERY: "utility:location",
+    FAILURE:          "utility:error",
+    RETURNED:         "utility:undo",
+    UNKNOWN:          "utility:help",
+};
+
+const STATUS_VARIANT_MAP = {
+    DELIVERED:        "success",
+    IN_TRANSIT:       "brand",
+    OUT_FOR_DELIVERY: "warning",
+    FAILURE:          "error",
+    RETURNED:         "error",
+    UNKNOWN:          "default",
+};
+
+const CARRIER_TRACKING_URL = {
+    USPS:    "https://tools.usps.com/go/TrackConfirmAction?tLabels=",
+    UPS:     "https://www.ups.com/track?tracknum=",
+    FEDEX:   "https://www.fedex.com/fedextrack/?trknbr=",
+    DHL:     "https://www.dhl.com/en/express/tracking.html?AWB=",
+    CORREOS: "https://www.correos.es/es/es/herramientas/localizador/envios/detalle?tracking-number=",
+};
+
 export default class OrderShipmentTracker extends LightningElement {
     @api recordId;
 
@@ -12,7 +38,6 @@ export default class OrderShipmentTracker extends LightningElement {
     wiredShipment({ data, error }) {
         this.isLoading = false;
         if (data) {
-            // Formateamos las fechas de los eventos para mostrarlas de forma legible
             this.shipment = {
                 ...data,
                 trackingEvents: data.trackingEvents.map(event => ({
@@ -22,7 +47,8 @@ export default class OrderShipmentTracker extends LightningElement {
                             day: '2-digit', month: '2-digit', year: 'numeric',
                             hour: '2-digit', minute: '2-digit'
                           })
-                        : ''
+                        : '',
+                    icon: STATUS_ICON_MAP[event.statusCode] || "utility:help"
                 }))
             };
             this.error = undefined;
@@ -42,6 +68,32 @@ export default class OrderShipmentTracker extends LightningElement {
 
     get hasTrackingEvents() {
         return this.shipment?.trackingEvents?.length > 0;
+    }
+
+    get currentStatus() {
+        if (!this.shipment?.trackingEvents?.length) {
+            return null;
+        }
+        return this.shipment.trackingEvents[0];
+    }
+
+    get currentStatusVariant() {
+        if (!this.currentStatus) {
+            return "default";
+        }
+        return STATUS_VARIANT_MAP[this.currentStatus.statusCode] || "default";
+    }
+
+    get currentStatusCssClass() {
+        return `status-${this.currentStatusVariant}`;
+    }
+
+    get trackingUrl() {
+        if (!this.shipment?.trackingNumber || !this.shipment?.carrier) {
+            return null;
+        }
+        const baseUrl = CARRIER_TRACKING_URL[this.shipment.carrier.toUpperCase()];
+        return baseUrl ? `${baseUrl}${this.shipment.trackingNumber}` : null;
     }
 
     handlePrintLabel() {
